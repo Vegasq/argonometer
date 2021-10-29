3c286d80b99178abdfa82dadb191117301bd0cb1
Nick Yakovliev <vegasq@protonmail.com>
2021-10-28 22:04:16 -0500

    Automatically run benchmark with a variety of workers

--- main.go
+++ main.go
@@ -3,19 +3,22 @@ package main
 import (
 	"fmt"
 	"github.com/matthewhartstonge/argon2"
+	"runtime/debug"
 	"time"
 )
 
 var HASH = []byte("$argon2id$v=19$m=65536,t=1,p=4$c2FsdHNhbHQ$RCeht9aM1+cvYpPovpMschlqMUf1vWgxUMuCDS1rMSM")
-
-const WORKERS = 100
 const TASKS = 1000000
 
-func worker(in chan int, result chan uint8) {
+func worker(in chan int, result chan bool, exit chan bool) {
 	for {
-		i := <-in
-		argon2.VerifyEncoded([]byte(fmt.Sprintf("plain%d", i)), HASH)
-		result <- 1
+		select {
+		case task := <- in:
+			argon2.VerifyEncoded([]byte(fmt.Sprintf("plain%d", task)), HASH)
+			result <- true
+		case <- exit:
+			return
+		}
 	}
 }
 
@@ -25,9 +28,10 @@ func dispatchTasks(tasks chan int) {
 	}
 }
 
-func startWorkers(tasks chan int, result chan uint8) {
-	for i := 0; i < WORKERS; i++ {
-		go worker(tasks, result)
+func startWorkers(workers int, tasks chan int, result chan bool, exit chan bool) {
+	//fmt.Printf("Start %d workers\n", workers)
+	for i := 0; i < workers; i++ {
+		go worker(tasks, result, exit)
 	}
 }
 
@@ -37,25 +41,37 @@ func sleep(start time.Time) {
 	}
 }
 
-func counter(c *int, result chan uint8) {
+func counter(c *int, result chan bool) {
 	for {
 		<-result
 		(*c) += 1
 	}
 }
 
-func main() {
+func benchmark(workers int) int {
 	var c int
 
 	tasks := make(chan int, TASKS)
-	result := make(chan uint8, TASKS)
+	result := make(chan bool, TASKS)
+	exit := make(chan bool)
 
 	go counter(&c, result)
 	start := time.Now()
 	dispatchTasks(tasks)
-	startWorkers(tasks, result)
+	startWorkers(workers, tasks, result, exit)
 	sleep(start)
+	for i := 0; i < workers; i++ {
+		exit <- true
+	}
 
-	fmt.Printf("Argon2id per minute:\t%d\n", c)
+	return c
+}
 
+
+func main(){
+	for i := 4; i <= 512; i = i*2 {
+		hpm := benchmark(i)
+		fmt.Printf("Workers: %d\tHashes: %d\n", i, hpm)
+		debug.FreeOSMemory()
+	}
 }
